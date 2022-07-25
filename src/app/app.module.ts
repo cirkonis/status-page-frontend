157c0f94c06eacbe6e79ab1529727891764259e7
mima <mima@computas.com>
2022-07-25 13:26:28 +0200

    small fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app.component';
 import { StatusPageComponent } from './status-page/status-page.component';
 import { HeaderComponent } from './status-page/header/header.component';
 import { FooterComponent } from './status-page/footer/footer.component';
-import { StatusTableComponent } from './status-page/status-table/status-table.component';
+import { StatusTableComponent } from './status-page/sli-status-table/status-table.component';
 import { UnmonitoredIconComponent } from './icons/unmonitored-icon.component';
 import { NormalIconComponent} from "./icons/normal-icon.component";
 import { InfoIconComponent } from './icons/info-icon.component';
@@ -13,7 +13,7 @@ import { WarningIconComponent } from './icons/warning-icon.component';
 import { ErrorIconComponent } from './icons/error-icon.component';
 import { AlertIconComponent } from './icons/alert-icon.component';
 import { PendingIconComponent } from "./icons/pending-icon.component";
-import { TableCellIconComponent } from './status-page/status-table/table-cell-icon/table-cell-icon.component';
+import { TableCellIconComponent } from './status-page/sli-status-table/table-cell-icon/table-cell-icon.component';
 import {HttpClientModule} from "@angular/common/http";
 import {CommonModule} from "@angular/common";
 
